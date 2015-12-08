88fa8078b94d21a398215b7257cdb3bc3141a635
ityler <tylernormile@gmail.com>
2015-12-07 20:36:34 -0500

    Updated day 5 Part 1 solution
    Day 5 part 1 only

--- Day_5/part1.pl
+++ Day_5/part1.pl
@@ -10,26 +10,26 @@ my($cond) = 0;                        # Condition init
 my($inf) = "input.txt";               # Input path/filename
 open(my $IN,"<",$inf)                 # Open input file
   or die "Unable to open input file!\n";
-open(my $LOG,">","log")               # Open log file
-  or die "Unable to open log file!\n";
 my(@strs) = <$IN>;                    # Slurp file into array
 close($IN);                           # Close input file
 foreach(@strs){                       # Each naughty or nice string           
-  $cond = 0; 
+  chomp($_);                          # Strip out crlf
   my($str) = $_;                      # String in array
   # Check bad requirements first
-  if(!($str =~ m/^(ab|cd|pq|xy)/)){   # Does not contain bad substring
+  if(!($str =~ m/(ab|cd|pq|xy)/)){   # Does not contain bad substring
     # Check for good requirements
     if($str =~ m/[aeiou].*[aeiou].*[aeiou]/){ # Contains 3 insances of voewls, probably a better way avail than repeating
-      print $LOG $str." Three vowels found\n";
-      if($str =~ /(.)\1/){            # Contains any duplicate characters
-        $cond = 1;                    # Condition is met
-      }
-      if($cond == 1){                 # Condition is true
-        $ns++;
+      # Must be double letter next to eachother not in entire string
+      my(@crarr) = split("",$str);     # Split string into char array
+      for(my $i = 0; $i <= $#crarr; $i++){  # Each character in array
+        if(exists($crarr[$i+1])){           # Next chracter in array exists(not the end of string) 
+          if($crarr[$i] eq $crarr[$i+1]){   # Does next character match currrent character
+            $ns++;                      # Increment nice string
+            last;                       # Break out of for loop
+          }  
+        }
       }
     } # Else bad string
   } # Else bad string
 }
-print "Nice strings: ".$ns."\n";      # Write out result 
-close($LOG);
+print "Nice strings: ".$ns."\n";      # Write out result
